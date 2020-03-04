c87d39c5dc492fef69c787998c9272dbce66c130
apinit <memories.cpem@gmail.com>
2020-03-04 23:48:17 +0700

    add route path component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
+import { ZooComponent } from './components/zoo/zoo.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'animal', pathMatch: 'full' },
+  { path: 'animal', component: AnimalComponent },
+  { path: 'zoo', component: ZooComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
